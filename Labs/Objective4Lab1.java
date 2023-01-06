9fc1a0731f5301496c4e67f130007519bb865eac
Kira Stout <kmstout08@gmail.com>
2023-01-06 17:54:56 -0500

    complete objective4lab1

---
+++ Labs/Objective4Lab1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Objective4Lab1 {
+  public static void main(String[] args) {
+
+    Scanner keyboard = new Scanner(System.in);
+
+    String fname = "Kira";
+    String lname = "Stout";
+    String favoriteAnimal = "Giraffe";
+    String favoriteFood = "Pulled Pork";
+    String favoriteSong = "Ain't Nobody";
+
+    System.out.println("What is your first name?");
+    String fname = input.nextLine()
+
+    System.out.println("What is your last name?");
+    String lname = input.nextLine()
+
+    System.out.println("What is your favorite animal?");
+    String favoriteAnimal = input.nextLine()
+
+    System.out.println("What is your favorite food?");
+    String favoriteFood input.nextLine()
+   
+  }
+}
