bc99ec380102fada91f7c9e6cb429fe140199aaa
Kira Stout <kmstout08@gmail.com>
2023-01-08 15:27:03 -0500

    complete objective4lab1

--- Labs/Objective4Lab1.java
+++ Labs/Objective4Lab1.java
@@ -1,27 +1,38 @@
-import java.util.Scanner;
+1import java.util.Scanner;
 
 public class Objective4Lab1 {
   public static void main(String[] args) {
 
     Scanner keyboard = new Scanner(System.in);
 
-    String fname = "Kira";
-    String lname = "Stout";
-    String favoriteAnimal = "Giraffe";
-    String favoriteFood = "Pulled Pork";
-    String favoriteSong = "Ain't Nobody";
+    String fname = "";
+    String lname = "";
+    String favoriteAnimal = "";
+    String favoriteFood = "";
+    String favoriteSong = "";
 
-    System.out.println("What is your first name?");
-    String fname = input.nextLine()
+    //TODO prompt the user to get input for all of the String variables.
+    system.out.println("What is your first name?");
+    String fname = input.nextLine();
 
-    System.out.println("What is your last name?");
-    String lname = input.nextLine()
+    system.out.println("What is your last name?");
+    String lname = input.nextLine();
 
-    System.out.println("What is your favorite animal?");
-    String favoriteAnimal = input.nextLine()
+    system.out.println("What is your favorite animal?");
+    String favoriteAnimal =input.nextLine();
 
-    System.out.println("What is your favorite food?");
-    String favoriteFood input.nextLine()
-   
+    system.out.println("What is your favorite food?");
+    String favoriteFood = input.nextLine();
+
+    system.out.println("What is your favorite song?");
+    String favoriteSong = input.nextLine();
+
+    system.out.println("My first name is" + fname);
+    system.out.println("My last name is" + lname);
+    system.out.println("My favorite animal is" + favoriteAnimal);
+    system.out.println("My favorite food is" + favoriteFood);
+    system.out.println("My favorite song is" + favoriteSong);
+    //TODO print the output formatted to look like the expected output using String concatenation.
+
   }
 }
